24c631f84f6d819f850f2a3a0e07e37c35c0e80a
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-19 13:07:59 +0530

    Added piece UI

---
+++ frontend/src/chess/ui/piece.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Image } from "react-konva";
+
+const Piece = (props) => {
+  // imageUrl
+  // color
+  // id
+  // -color of player
+  // -whether or not it's your turn
+  // -if this player's turn, color of this player is the samecolor as this piece
+  //     then we can make this piece draggable / moveable
+
+  return <Image draggable />;
+};
+
+export default Piece;
