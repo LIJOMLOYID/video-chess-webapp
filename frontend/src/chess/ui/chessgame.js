a6ae16c431e16e33b2302d95aaa6a71e63f410d8
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-19 13:25:37 +0530

    Architectured the chessgame

--- frontend/src/chess/ui/chessgame.js
+++ frontend/src/chess/ui/chessgame.js
@@ -1,6 +1,32 @@
 import React from "react";
 import Game from "../model/chess";
 
-class ChessGame extends React.Component {}
+class ChessGame extends React.Component {
+  state = {
+    gameState: new Game(this.props.isWhite),
+    whiteKingInCheck: false,
+    blackKingInCheck: false,
+  };
+
+  render() {
+    /**
+     * <div background = chessboard>
+     *  <stage>
+     *      <layer for loop model and renders it>
+     *
+     *
+     * <div>
+     */
+  }
+
+  movePiece(selectedId, finalPosition) {
+    /**
+     * uses pythagorean theorem to calculate
+     * the distance between the final position of the
+     * chess pieces and every square on the board,
+     * and assigns the piece to the closest square
+     */
+  }
+}
 
 export default ChessGame;
