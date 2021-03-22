1c84ca8de53662eb23d1dcb4e8483ea4fc428f51
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-22 17:49:36 +0530

    Implemented the basic move logic

--- frontend/src/chess/model/chessPiece.js
+++ frontend/src/chess/model/chessPiece.js
@@ -9,10 +9,31 @@ class ChessPiece {
   setSquare(newSquare) {
     // assign this piece to a specific square
     // this.square = square
+    // console.log(newSquare)
+    if (newSquare === undefined) {
+      this.squareThisPieceIsOn = newSquare;
+      return;
+    }
+
+    if (this.squareThisPieceIsOn === undefined) {
+      this.squareThisPieceIsOn = newSquare;
+      newSquare.setPiece(this);
+    }
+
+    const isNewSquareDifferent =
+      this.squareThisPieceIsOn.x != newSquare.x ||
+      this.squareThisPieceIsOn.y != newSquare.y;
+
+    if (isNewSquareDifferent) {
+      // console.log("set")
+      this.squareThisPieceIsOn = newSquare;
+      newSquare.setPiece(this);
+    }
   }
 
   getSquare() {
     // get the current square this piece is on
     // undefined is this piece is not on the board.
+    return this.squareThisPieceIsOn;
   }
 }
--- frontend/src/chess/ui/chessgame.js
+++ frontend/src/chess/ui/chessgame.js
@@ -1,12 +1,44 @@
 import React from "react";
 import Game from "../model/chess";
+import Square from "../model/square";
+import { Stage, Layer } from "react-konva";
+import Board from "../assets/chessBoard.png";
+//import useSound from "use-sound";
+//import socket from "../../connection/socket";
+import Piece from "./piece";
+import piecemap from "./piecemap";
+//import { useParams } from "react-router-dom";
+// import { ColorContext } from "../../context/colorcontext";
+// import VideoChatApp from "../../connection/videochat"; // we gotta make the video app first
+// import socket from ""; // here is where we would initialize the socket logic.
 
 class ChessGame extends React.Component {
   state = {
     gameState: new Game(this.props.isWhite),
+    draggedPieceTargetId: "", // empty string means no piece is being dragged
+    draggedPieceTargetId: "", // empty string means no piece is being dragged
     whiteKingInCheck: false,
     blackKingInCheck: false,
   };
+
+  componentDidMount() {
+    // here we need to register event listeners for socket.io
+    // socket.on("opponent move", (move) => {
+    //   // move == [pieceId, finalPosition]
+    //   // console.log("opponenet's move: " + move.selectedId + ", " + move.finalPosition)
+    //   if (move.playerColorThatJustMovedIsWhite !== this.props.color) {
+    //     this.movePiece(
+    //       move.selectedId,
+    //       move.finalPosition,
+    //       this.state.gameState,
+    //       false
+    //     );
+    //     this.setState({
+    //       playerTurnToMoveIsWhite: !move.playerColorThatJustMovedIsWhite,
+    //     });
+    //   }
+    // });
+  }
 
   render() {
     /**
@@ -17,6 +49,52 @@ class ChessGame extends React.Component {
      *
      * <div>
      */
+    return (
+      <>
+        <div
+          style={{
+            backgroundImage: `url(${Board})`,
+            width: "720px",
+            height: "720px",
+          }}
+        >
+          <Stage width={1000} height={1000}>
+            <Layer>
+              {this.state.gameState.getBoard().map((row) => {
+                return (
+                  <>
+                    {row.map((square) => {
+                      if (square.isOccupied()) {
+                        return (
+                          <Piece
+                            x={square.getCanvasCoord()[0]}
+                            y={square.getCanvasCoord()[1]}
+                            imgurls={piecemap[square.getPiece().name]}
+                            isWhite={square.getPiece().color === "white"}
+                            thisPieceTargetId={square.getPiece().targetId}
+                            draggedPieceTargetId={
+                              this.state.draggedPieceTargetId
+                            }
+                            onDragStart={this.startDragging}
+                            onDragEnd={this.endDragging}
+                            id={square.getPieceIdOnThisSquare()}
+                            thisPlayersColorIsWhite={this.props.color}
+                            playerTurnToMoveIsWhite={
+                              this.state.playerTurnToMoveIsWhite
+                            }
+                          />
+                        );
+                      }
+                      return;
+                    })}
+                  </>
+                );
+              })}
+            </Layer>
+          </Stage>
+        </div>
+      </>
+    );
   }
 
   movePiece(selectedId, finalPosition) {
